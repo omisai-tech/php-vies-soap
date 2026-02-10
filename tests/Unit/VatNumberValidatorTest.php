b0a9fb9387a234a62b006ec5c257df6d1792d8ca
Sándor Gergely Bornemisza <bornemisza@omisai.com>
2026-02-10 21:08:14 +0100

    style: remove unnecessary parentheses in VatNumberValidatorTest for consistency

--- tests/Unit/VatNumberValidatorTest.php
+++ tests/Unit/VatNumberValidatorTest.php
@@ -4,7 +4,7 @@ use Omisai\ViesSoap\Exceptions\ViesValidationException;
 use Omisai\ViesSoap\Validation\VatNumberValidator;
 
 it('validates country codes and VAT numbers', function () {
-    $validator = new VatNumberValidator();
+    $validator = new VatNumberValidator;
 
     [$countryCode, $vatNumber] = $validator->validate('de', '123456789');
 
@@ -13,13 +13,13 @@ it('validates country codes and VAT numbers', function () {
 });
 
 it('rejects invalid country codes', function () {
-    $validator = new VatNumberValidator();
+    $validator = new VatNumberValidator;
 
     $validator->validateCountryCode('D1');
 })->throws(ViesValidationException::class);
 
 it('rejects invalid VAT numbers', function () {
-    $validator = new VatNumberValidator();
+    $validator = new VatNumberValidator;
 
     $validator->validateVatNumber('!@#');
 })->throws(ViesValidationException::class);
