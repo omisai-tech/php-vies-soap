d2431d5e846bd651dd0617868267edfb2a4004b8
Sándor Gergely Bornemisza <bornemisza@omisai.com>
2026-02-10 21:07:23 +0100

    style: remove unnecessary PHPDoc annotations from SoapClientInterface

--- src/Contracts/SoapClientInterface.php
+++ src/Contracts/SoapClientInterface.php
@@ -4,9 +4,7 @@ namespace Omisai\ViesSoap\Contracts;
 
 interface SoapClientInterface
 {
-    /** @return object */
     public function checkVat(array $params): object;
 
-    /** @return object */
     public function checkVatApprox(array $params): object;
 }
